8402fc809d6c95ce2731c8e1eaa768532f02556c
Shawn Berg <bergshawnm@gmail.com>
2018-12-29 21:34:51 -0600

    Stubbed out programs and functions
    Stubbed out programs and functions for controlling the lights

---
+++ control.py
@@ -0,0 +1,7 @@
+
+# for use if the light is off
+def setDef:
+
+# for used if the light is on
+def setLight:
+
---
+++ sunrise.py
@@ -0,0 +1,11 @@
+# if light unresponsive
+    #inf loop and wait for response
+
+# if light is responsive but off 
+    setDef
+
+# if light is responsive and on
+    setlight
+    setDef
+
+
