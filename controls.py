caa384408ffa48d4e77ecd84e922f9b4015dbd07
Shawn Berg <bergshawnm@gmail.com>
2018-12-30 23:58:02 -0600

    Finally got the JSON for the POST request to work!
    Apparently the JSON format for the bulb needs the escape sequence '\"' passed through so I had to escape the escape character (e.g. '\\\"') HUGE pain debugging that, but its fixed.

--- controls.py
+++ controls.py
@@ -2,13 +2,17 @@ import requests
 
 url = "https://wap.tplinkcloud.com?token=ddc8c82d-A78UG1SO4ua7acaQsH2W1rU"
 
-header = {'Content-Type': 'application/json'}
+header = {"Content-Type": "application/json"}
 
 # for use if the light is off
 def setDef(index):
-    r = requests.post(url, data={"method":"passthrough","params":{"deviceId":"8012CFBDD636A8E1C9B2248B3850543B19C8665F","requestData":{"smartlife.iot.smartbulb.lightingservice":{"set_default_behavior":{"soft_on":{"mode":"customize_preset","index":3,"hue":0,"saturation":0,"color_temp":2500,"brightness":30}}}}}, headers=header)
-    print(r.status_code)
-    print(r.text)
+    #hard on (with switch)
+    data = '{"method":"passthrough", "params": {"deviceId": "8012CFBDD636A8E1C9B2248B3850543B19C8665F", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_default_behavior\\\":{\\\"hard_on\\\":{\\\"mode\\\":\\\"customize_preset\\\",\\\"index\\\":3}}}}" }}'
+    r = requests.post(url, data=data, headers=header)
+    
+    #soft on (with app or alexa)
+    data = '{"method":"passthrough", "params": {"deviceId": "8012CFBDD636A8E1C9B2248B3850543B19C8665F", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_default_behavior\\\":{\\\"soft_on\\\":{\\\"mode\\\":\\\"customize_preset\\\",\\\"index\\\":3}}}}" }}'
+    r = requests.post(url, data=data, headers=header)
 
 # for used if the light is on
 #def setLight():
