a74eddf4c7baed1f7574e2290e9559208bdc3b00
Shawn Berg <bergshawnm@gmail.com>
2019-01-09 15:56:24 -0600

    Added command for setting presets and modified data variable
    Successfully added the POST request for updating the preset for the bulbs, also modified the data variable to allow for variables to be placed within the string.

--- controls.py
+++ controls.py
@@ -1,23 +1,40 @@
 import requests
 
+bulb1 = "8012CFBDD636A8E1C9B2248B3850543B19C8665F"
+
 url = "https://wap.tplinkcloud.com?token=ddc8c82d-A78UG1SO4ua7acaQsH2W1rU"
 
 header = {"Content-Type": "application/json"}
 
-# for use if the light is off
-def setDef(index):
-    #hard on (with switch)
-    data = '{"method":"passthrough", "params": {"deviceId": "8012CFBDD636A8E1C9B2248B3850543B19C8665F", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_default_behavior\\\":{\\\"hard_on\\\":{\\\"mode\\\":\\\"customize_preset\\\",\\\"index\\\":3}}}}" }}'
-    r = requests.post(url, data=data, headers=header)
-    
-    #soft on (with app or alexa)
-    data = '{"method":"passthrough", "params": {"deviceId": "8012CFBDD636A8E1C9B2248B3850543B19C8665F", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_default_behavior\\\":{\\\"soft_on\\\":{\\\"mode\\\":\\\"customize_preset\\\",\\\"index\\\":3}}}}" }}'
-    r = requests.post(url, data=data, headers=header)
+def debug(request):
+    print(request.text)
 
-# for used if the light is on
+# change the default behavior of the light bulb when turned on by switch to a preset index
+def setDefHard(index):
+    #hard on (with switch)
+    data = '{"method":"passthrough", "params": {"deviceId": "8012CFBDD636A8E1C9B2248B3850543B19C8665F", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_default_behavior\\\":{\\\"hard_on\\\":{\\\"mode\\\":\\\"customize_preset\\\",\\\"index\\\":1}}}}" }}'
+    r = requests.post(url, data=data, headers=header)
+    debug(r)
+    
+# change the default behavior of the light bulb when turned on by software to a preset index
+def setDefSoft(index):
+    #soft on (with app or alexa)
+    data = '{"method":"passthrough", "params": {"deviceId": "8012CFBDD636A8E1C9B2248B3850543B19C8665F", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_default_behavior\\\":{\\\"soft_on\\\":{\\\"mode\\\":\\\"customize_preset\\\",\\\"index\\\":1}}}}" }}'
+    r = requests.post(url, data=data, headers=header)
+    debug(r)
+
+# change the light
 #def setLight():
+
+# change a preset
+def setPreset(index, temp, brightness):
+    data = '{"method":"passthrough", "params": {"deviceId": "' + bulb1 + '", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_preferred_state\\\":{\\\"saturation\\\":0,\\\"index\\\":1,\\\"hue\\\":0,\\\"color_temp\\\":3800,\\\"brightness\\\":80}}}" }}'
+    r = requests.post(url, data=data, headers=header)
+    debug(r)
 
 # used to get the current status of the light
 #def getStatus():
 
-setDef(1)
+setDefHard(1)
+setDefSoft(1)
+setPreset(0, 3800, 80)
