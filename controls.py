13e0762b5fab3c3de2eee820ade3112407f76a60
Shawn Berg <bergshawnm@gmail.com>
2019-01-09 16:09:57 -0600

    Updated data strings in controls.py
    I updated the data strings in controls.py to allow for using variables

--- controls.py
+++ controls.py
@@ -10,16 +10,16 @@ def debug(request):
     print(request.text)
 
 # change the default behavior of the light bulb when turned on by switch to a preset index
-def setDefHard(index):
+def setDefHard(bulb, index):
     #hard on (with switch)
-    data = '{"method":"passthrough", "params": {"deviceId": "8012CFBDD636A8E1C9B2248B3850543B19C8665F", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_default_behavior\\\":{\\\"hard_on\\\":{\\\"mode\\\":\\\"customize_preset\\\",\\\"index\\\":1}}}}" }}'
+    data = '{"method":"passthrough", "params": {"deviceId": "' + bulb + '", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_default_behavior\\\":{\\\"hard_on\\\":{\\\"mode\\\":\\\"customize_preset\\\",\\\"index\\\":' + str(index) + '}}}}" }}'
     r = requests.post(url, data=data, headers=header)
     debug(r)
     
 # change the default behavior of the light bulb when turned on by software to a preset index
-def setDefSoft(index):
+def setDefSoft(bulb, index):
     #soft on (with app or alexa)
-    data = '{"method":"passthrough", "params": {"deviceId": "8012CFBDD636A8E1C9B2248B3850543B19C8665F", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_default_behavior\\\":{\\\"soft_on\\\":{\\\"mode\\\":\\\"customize_preset\\\",\\\"index\\\":1}}}}" }}'
+    data = '{"method":"passthrough", "params": {"deviceId": "' + bulb + '", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_default_behavior\\\":{\\\"soft_on\\\":{\\\"mode\\\":\\\"customize_preset\\\",\\\"index\\\":' + str(index) + '}}}}" }}'
     r = requests.post(url, data=data, headers=header)
     debug(r)
 
@@ -27,14 +27,14 @@ def setDefSoft(index):
 #def setLight():
 
 # change a preset
-def setPreset(index, temp, brightness):
-    data = '{"method":"passthrough", "params": {"deviceId": "' + bulb1 + '", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_preferred_state\\\":{\\\"saturation\\\":0,\\\"index\\\":1,\\\"hue\\\":0,\\\"color_temp\\\":3800,\\\"brightness\\\":80}}}" }}'
+def setPreset(bulb, index, temp, brightness):
+    data = '{"method":"passthrough", "params": {"deviceId": "' + bulb + '", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"set_preferred_state\\\":{\\\"saturation\\\":0,\\\"index\\\":' + str(index) + ',\\\"hue\\\":0,\\\"color_temp\\\":' + str(temp) + ',\\\"brightness\\\":' + str(brightness) + '}}}" }}'
     r = requests.post(url, data=data, headers=header)
     debug(r)
 
 # used to get the current status of the light
 #def getStatus():
 
-setDefHard(1)
-setDefSoft(1)
-setPreset(0, 3800, 80)
+setDefHard(bulb1, 1)
+setDefSoft(bulb1, 1)
+setPreset(bulb1, 0, 3800, 80)
