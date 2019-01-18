356f59a90758f9be60804ac94698c157fa299264
Shawn Berg <bergshawnm@gmail.com>
2019-01-17 23:25:01 -0600

    Sunrise.py is now working for when the lights are on
    sunrise.py now currently runs correctly with the lights on. All of the functions in controls.py should now work properly. Further testing is needed for the lights when they are off.

--- controls.py
+++ controls.py
@@ -3,12 +3,20 @@ import json
 import socket
 
 bulb1 = "8012CFBDD636A8E1C9B2248B3850543B19C8665F"
+bulb2 = "8012B569B7255CD98542E8D6F1F308A319C7ACC6"
+bulb3 = "80121EE053655BB04B5D29A83226E69E19C65783"
+
+
 url = "https://wap.tplinkcloud.com?token=ddc8c82d-A3pashRN9PvhV4s6HmfJyVe"
 
 header = {"Content-Type": "application/json"}
 
 def debug(request):
     print(request.text)
+
+def setDef(bulb, index):
+    setDefHard(bulb, index)
+    setDefSoft(bulb, index)
 
 # change the default behavior of the light bulb when turned on by switch to a preset index
 def setDefHard(bulb, index):
@@ -25,8 +33,10 @@ def setDefSoft(bulb, index):
     #debug(r)
 
 # change the light
-def setLight(bulb, temp, brightness):
-    data = '{"method":"passthrough", "params": {"deviceId": "' + bulb + '", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"transition_light_state\\\":{\\\"ignore_default\\\":1, \\\"on_off\\\":1, \\\"color_temp\\\":' + str(temp) + ', \\\"brightness\\\":' + str(brightness) + '}}}" }}'
+# transition time should be in seconds
+def setLight(bulb, trans, temp, brightness):
+    trans = trans * 1000
+    data = '{"method":"passthrough", "params": {"deviceId": "' + bulb + '", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"transition_light_state\\\":{\\\"ignore_default\\\":1, \\\"transition_period\\\":'+ str(trans) + ', \\\"on_off\\\":1, \\\"color_temp\\\":' + str(temp) + ', \\\"brightness\\\":' + str(brightness) + '}}}" }}'
     r = requests.post(url, data=data, headers=header)
     #debug(r)
 
@@ -47,8 +57,8 @@ def getStatus(bulb):
         on_off = bulbStatus['smartlife.iot.smartbulb.lightingservice']['get_light_state']['on_off']
         temp = bulbStatus['smartlife.iot.smartbulb.lightingservice']['get_light_state']['color_temp']
         brightness = bulbStatus['smartlife.iot.smartbulb.lightingservice']['get_light_state']['brightness']
-        print(on_off, temp, brightness)
-        return{on_off, temp, brightness}
+        #print(on_off, temp, brightness)
+        return[on_off, temp, brightness]
     else:
         return "error"
 
@@ -61,11 +71,23 @@ def initDev():
     f = open('/home/pi/Circadian-Lights/devices.list', 'w+')
     for each in response['result']['deviceList']:
         f.write(each['deviceId'] + "\n")
-    debug(r)
+    #debug(r)
 
+
+#debug controls000
 #initDev()
-getStatus(bulb1)
-#setLight(bulb1, 3545, 75)
+#getStatus(bulb1)
+#setLight(bulb1, 300, 3800, 5)
+#getStatus(bulb1)
+#getStatus(bulb2)
+#setLight(bulb2, 300, 3800, 5)
+#getStatus(bulb2)
+#getStatus(bulb3)
+#setLight(bulb3, 1, 3800, 5)
+#getStatus(bulb3)
+#getStatus(bulb1)
+#setLight(bulb1, 300, 3734, 76)
+#getStatus(bulb1)
 #setDefHard(bulb1, 1)
 #setDefSoft(bulb1, 1)
 #setPreset(bulb1, 0, 3800, 80)
--- sunrise.py
+++ sunrise.py
@@ -1,12 +1,27 @@
-import controls.py 
+import controls as controls
 import time 
+import os 
+import json 
 
+
+# load devices in from file
 def loadDev():
-    try:
+
+    if os.path.exists("/home/pi/Circadian-Lights/devices.list"):
+        f = open("/home/pi/Circadian-Lights/devices.list", "r")
+        bulbs = f.read().splitlines()
+    else:
+        controls.initDev()
+        f = open("/home/pi/Circadian-Lights/devices.list", "r")
+        bulbs = f.read().splitlines()
+
+    return bulbs
+
+    """try:
         #open device list file
         f = open("/home/pi/Circadian-Lights/device.list", "r")
 
-    except:#TODO exception for no file
+    except IOerror:
         # if no file
         controls.initDev()
         #open file
@@ -14,38 +29,130 @@ def loadDev():
 
     finally:
         # load file into list
-        bulbs = f.readlines()
+        f = open("/home/pi/Circadian-Lights/device.list", "r")
+        bulbs = f.readlines()"""
 
-def changeLight(bulb, targetTemp, targetBrightness):
-    status = controls.getStatus()
+# load the relevant values in from the file
+def loadStates():
+    if os.path.exists("/home/pi/Circadian-Lights/values.target"):
+        f = open("/home/pi/Circadian-Lights/values.target", "r")
+        #text = f.readlines()
+        #print(text)
+        states = json.loads(f.read())
+    else:
+        #controls.initDev()
+        f = open("/home/pi/Circadian-Lights/values.target", "r")
+        states = json.loads(f.read())
+
+    return states
+    
+    #try:
+    #    #open states file
+    #    f = open("/home/pi/Circadian-Lights/values.target", "r")
+
+    #except:
+    #    print("could not open values.target")
+
+    #finally:
+    #    #load file into list
+    #    text = f.readlines()
+    #    states = json.loads(text)
+
+# change the light
+def changeLight(interval, targetTemp, targetBrightness, final):
+    status = controls.getStatus(bulbs[0])
+    count = 0
     # if light unresponsive and last change
-    if status == "error" and currTemp + tempInc == targetTemp: 
+    if status == "error" and final == True: 
         # inf loop and wait to make change
         while(status == "error"):
             #TODO change to as fast as possible once we network directly
             time.sleep(1)
-            status = controls.getStatus()
-    elif status == "error" and currTemp + tempInc < targetTemp:
-        return
+            if count < interval:
+                count+=1
+            status = controls.getStatus(bulbs[0])
 
+    # if light unresponsive and not last change
+    elif status == "error" and final == False:
+        # wait for the specifed time interval
+        while(count < interval):
+            if status == "error":
+                time.sleep(1)
+                count+=1
+            # if light comes on, change it
+            elif status != "error":
+                break
+            # if light doesn't come on during interval, skip
+            if count >= interval:
+                return
+    
+    # if light responsive and off
     if status[0] == 0:
-        controls.setDef(bulb, 0, temp, brightness)
-
+        # set light to be target next time turned on
+        for bulb in bulbs:
+            controls.setPreset(bulb, 0, targetTemp, targetBrightness)
+            controls.setDef(bulb, 0)
+        # wait for next command
+        if count < interval:
+            time.sleep(interval-count)
+    
+    # if light responsive and on
     if status[0] == 1:
-        controls.setlight(bulb, temp, brightness)
-        controls.setDef(bulb, 0, temp, brightness)
+        for bulb in bulbs:
+            # transition light over specified length of time
+            controls.setLight(bulb, interval-count, targetTemp, targetBrightness)
+            # set light to be target next time turned on
+            controls.setPreset(bulb, 0, targetTemp, targetBrightness)
+            controls.setDef(bulb, 0)
+        # wait for next command
+        if count < interval:
+            time.sleep(interval-count)
 
 # slowly transition the light from night to daytime
-def transition():
-    # get current bulb settings, specifically brightness and temp
+# send 12 commands over an hour to transition light
+def transition(bulbs, states):
+    # get last status of lights
+    status = controls.getStatus(bulbs[0]) 
+
+    # read values for next state from file
+    targetTemp = states['Midday(WD)']['Temp'] 
+    targetBrightness = states['Midday(WD)']['Brightness'] 
+
+    # if light off
+    if status == "error" or status[0] == 0:
+        # use night values for curr values
+        currTemp = target['Night']['Temp']
+        currBrightness = target['Night']['Brightness']
+    # light must be on so use current values 
+    else:
+        currTemp = status[1]
+        currBrightness = status[2]
+
+    # interval in seconds
+    interval = 300
+    tempInt = (targetTemp - currTemp)/12
+    nextTemp = currTemp + tempInt
     
+    brightInt = (targetBrightness - currBrightness)/12
+    nextBrightness = currBrightness + brightInt
 
     # while current bulb brightness and temp != daytime brightness and temp
-        #TODO change to forking or threading for speed
-        for bulb in bulbs:
-            # pass device ID
-            changeLight(bulb, temp, brightness)
-#loadDev()
-#transition()
+    final = False
+    for i in range(12):
+        if i == 11:
+            final = True
+        # change the lights
+        changeLight(interval, nextTemp, nextBrightness, final)
+
+        #print("currTemp = " + str(currTemp) + ", currBrightness = " + str(currBrightness))
+        # set the next temps and brightnesses
+        nextTemp = currTemp + tempInt
+        nextBrightness = currBrightness + brightInt
+        #print("nextTemp = " + str(nextTemp) + ", nextBrightness = " + str(nextBrightness))
+        currTemp = nextTemp
+        currBrightness = nextBrightness
 
 
+bulbs = loadDev()
+states = loadStates()
+transition(bulbs, states)
