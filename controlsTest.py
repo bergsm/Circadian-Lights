81121b8d41a317aca6048b0b95708a7d4165eb7a
Shawn Berg <bergshawnm@gmail.com>
2019-01-28 20:49:20 -0600

    Testing out sending commands directly to bulbs

--- controlsTest.py
+++ controlsTest.py
@@ -1,11 +1,35 @@
 import requests
 import json
 import socket
+from struct import pack
 
 bulb1 = "8012CFBDD636A8E1C9B2248B3850543B19C8665F"
 bulb2 = "8012B569B7255CD98542E8D6F1F308A319C7ACC6"
 bulb3 = "80121EE053655BB04B5D29A83226E69E19C65783"
 
+bulb1IP = "192.168.1.143"
+bulb2IP = "192.168.1.102"
+bulb3IP = "192.168.1.121"
+
+PORT = 9999
+
+def encrypt(string):
+    key = 171
+    result = pack('>I', len(string))
+    for i in string:
+        a = key ^ ord(i)
+        key = a
+        result += chr(a)
+    return result
+
+def decrypt(string):
+    key = 171
+    result = ""
+    for i in string:
+        a = key ^ ord(i)
+        key = ord(i)
+        result += chr(a)
+    return result
 
 url = "https://wap.tplinkcloud.com?token=ddc8c82d-A3pashRN9PvhV4s6HmfJyVe"
 
@@ -35,11 +59,30 @@ def setDefSoft(bulb, index):
 # change the light
 # transition time should be in seconds
 def setLight(bulb, trans, temp, brightness):
-    #TODO change to direct message with bulb via socket
     trans = trans * 1000
     data = '{"method":"passthrough", "params": {"deviceId": "' + bulb + '", "requestData": "{\\\"smartlife.iot.smartbulb.lightingservice\\\":{\\\"transition_light_state\\\":{\\\"ignore_default\\\":1, \\\"transition_period\\\":'+ str(trans) + ', \\\"on_off\\\":1, \\\"color_temp\\\":' + str(temp) + ', \\\"brightness\\\":' + str(brightness) + '}}}" }}'
     r = requests.post(url, data=data, headers=header)
     debug(r)
+
+def setLightDirect(bulb, trans, temp, brightness):
+    #TODO change to direct message with bulb via socket
+    trans = trans * 1000
+    data = '{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1, "transition_period":' + str(trans) + ', "on_off":1, "color_temp":' + str(temp) + ', "brightness":' + str(brightness) + '}}}'
+
+
+    try:
+        s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((bulb, PORT))
+        s.send(encrypt(data))
+        r = s.recv(2048)
+        print(decrypt(r[4:]))
+        s.close()
+    except socket.error:
+        print("Could not connect to host " + bulb1IP + ":" + str(port))
+        
+    
+    #r = requests.post(url, data=data, headers=header)
+
 
 # change a preset
 def setPreset(bulb, index, temp, brightness):
--- debug.py
+++ debug.py
@@ -6,6 +6,10 @@ bulb2 = "8012B569B7255CD98542E8D6F1F308A319C7ACC6"
 bulb3 = "80121EE053655BB04B5D29A83226E69E19C65783"
 
 
+bulb1IP = "192.168.1.143"
+bulb2IP = "192.168.1.102"
+bulb3IP = "192.168.1.121"
+
 url = "https://wap.tplinkcloud.com?token=ddc8c82d-A3pashRN9PvhV4s6HmfJyVe"
 
 header = {"Content-Type": "application/json"}
@@ -17,10 +21,13 @@ header = {"Content-Type": "application/json"}
 #controls.setLight(bulb1, 1, 2875, 80)
 #getStatus(bulb1)
 #getStatus(bulb2)
-controls.setLight(bulb2, 1, 3800, 80)
-controls.setPreset(bulb2, 0, 3800, 80)
-controls.setDefHard(bulb2, 0)
-controls.setDefSoft(bulb2, 0)
+#controls.setLight(bulb2, 1, 3800, 80)
+controls.setLightDirect(bulb1IP, 1, 3800, 80)
+controls.setLightDirect(bulb2IP, 1, 3800, 80)
+controls.setLightDirect(bulb3IP, 1, 3800, 80)
+#controls.setPreset(bulb2, 0, 3800, 80)
+#controls.setDefHard(bulb2, 0)
+#controls.setDefSoft(bulb2, 0)
 #getStatus(bulb2)
 #getStatus(bulb3)
 #controls.setLight(bulb3, 1, 3800, 80)
