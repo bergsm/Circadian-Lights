2423778366282fe245c0666184b273678f735784
Shawn Berg <bergshawnm@gmail.com>
2019-02-02 22:36:54 -0600

    Updated debug.py with socket comms
    Removed HTTP requests from debug

--- debug.py
+++ debug.py
@@ -1,14 +1,9 @@
-#import controls as controls
-import controlsTest as controls
-
-bulb1 = "8012CFBDD636A8E1C9B2248B3850543B19C8665F"
-bulb2 = "8012B569B7255CD98542E8D6F1F308A319C7ACC6"
-bulb3 = "80121EE053655BB04B5D29A83226E69E19C65783"
+import controls as controls
 
 
-bulb1IP = "192.168.1.143"
-bulb2IP = "192.168.1.102"
-bulb3IP = "192.168.1.121"
+bulb1 = "192.168.1.143"
+bulb2 = "192.168.1.102"
+bulb3 = "192.168.1.121"
 
 url = "https://wap.tplinkcloud.com?token=ddc8c82d-A3pashRN9PvhV4s6HmfJyVe"
 
@@ -17,11 +12,12 @@ header = {"Content-Type": "application/json"}
 
 #debug controls
 #initDev()
-controls.getStatus(bulb1)
+controls.getStatus(bulb3)
 #controls.setLight(bulb1, 1, 2875, 80)
 #getStatus(bulb1)
 #getStatus(bulb2)
-#controls.setLight(bulb2, 1, 3800, 80)
+controls.setLight(bulb3, 1, 2700, 1)
+controls.getStatus(bulb3)
 #controls.setPreset(bulb2, 0, 3800, 80)
 #controls.setDefHard(bulb2, 0)
 #controls.setDefSoft(bulb2, 0)
