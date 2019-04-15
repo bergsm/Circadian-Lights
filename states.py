089b582580e3c5155674673109d9c791590a5005
Shawn Berg <bergshawnm@gmail.com>
2019-04-15 13:17:08 -0500

    removed testing file

--- states.py
+++
@@ -1,9 +0,0 @@
-import utils
-
-states = utils.loadStates()
-
-# get last status of lights
-
-# read values for next state from file
-targetTemp = states['Night']['Temp']
-targetBrightness = states['Night']['Brightness']
